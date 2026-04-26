b55acb3bc700a24322b06fd64234eae064c54a00
Eugene Ang <eugeneang654871@gmail.com>
2026-04-26 13:56:30 +0800

    Add installation note for PSFzf keybindings
    Added comment to instruct user to install PSFzf module.

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 Invoke-Expression (&starship init powershell)
 
+# Run Install-Module PSFzf -Scope CurrentUser first for fzf keybindings
 Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r'
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t'
